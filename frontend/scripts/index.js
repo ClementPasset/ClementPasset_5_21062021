a1c26095c13b44b597876fc4f199e5a1749aa53d
melch <clement.passet@gmail.com>
2021-06-22 18:43:53 +0200

    Contenu de la page produit

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,57 +1,71 @@
 const main = document.querySelector('.main');
 const address = "http://localhost:3000/api/teddies/";
 
+//Récupère les éléments en appelant l'API et retourne une Promise
 const getItems = () => {
     return new Promise((resolve, reject) => {
         fetch(address)
             .then(response => response.json())
             .then(data => resolve(data))
-            .catch(() => reject())
+            .catch(err => reject(err))
     });
 };
 
+//Affiche une élément dans le container passé en paramètre
+const displayItem = (item, container) => {
+    newCard = document.createElement('article');
+    newCard.classList.add('card');
+
+    newImg = document.createElement('img');
+    newImg.classList.add('card__img');
+    newImg.setAttribute('src', item.imageUrl);
+    newCard.appendChild(newImg);
+
+    newTitle = document.createElement('h3');
+    newTitle.classList.add('card__title');
+    newTitle.innerHTML = item.name + ' - ' + (item.price / 100).toFixed(2).replace('.', ',') + ' €';
+    newCard.appendChild(newTitle);
+
+    newDescription = document.createElement('p');
+    newDescription.classList.add('card__description');
+    newDescription.innerHTML = item.description;
+    newCard.appendChild(newDescription);
+
+    newLink = document.createElement('a');
+    newLink.setAttribute('href', 'product.html?id=' + item._id);
+    newCard.appendChild(newLink);
+
+    newButton = document.createElement('button');
+    newButton.classList.add('button');
+    newButton.innerHTML = 'Voir +';
+    newLink.appendChild(newButton);
+
+    container.appendChild(newCard);
+};
+
+
 getItems()
     .then(items => {
+
         container = document.createElement('section');
-        container.classList.add("main__cardContainer")
+        container.classList.add("main__cardContainer");
         main.appendChild(container);
 
+        title = document.createElement('h2');
+        title.classList.add("main__title");
+        title.innerHTML = "Tous nos Nounours";
+        container.appendChild(title);
+
+
         items.forEach(item => {
-            newCard = document.createElement('article');
-            newCard.classList.add('card');
-
-            newImg = document.createElement('img');
-            newImg.classList.add('card__img');
-            newImg.setAttribute('src', item.imageUrl);
-            newCard.appendChild(newImg);
-
-            newTitle = document.createElement('h3');
-            newTitle.classList.add('card__title');
-            newTitle.innerHTML = item.name;
-            newCard.appendChild(newTitle);
-
-            newDescription = document.createElement('p');
-            newDescription.classList.add('card__description');
-            newDescription.innerHTML = item.description;
-            newCard.appendChild(newDescription);
-
-            newLink = document.createElement('a');
-            newLink.setAttribute('href', 'product.html?id=' + item._id);
-            newCard.appendChild(newLink);
-
-            newButton = document.createElement('button');
-            newButton.classList.add('button');
-            newButton.innerHTML = 'Voir +';
-            newLink.appendChild(newButton);
-
-            container.appendChild(newCard);
+            displayItem(item, container);
         })
     })
-    .catch(() => {
+    .catch((err) => {
         errorTitle = document.createElement('h2');
-        errorTitle.innerHTML = 'Une erreur est survenue.'
+        errorTitle.innerHTML = 'Une erreur est survenue.';
         errorText = document.createElement('p');
-        errorText.innerHTML = 'Il y a eu un problème de communication avec le serveur, merci de réessayer plus tard.'
+        errorText.innerHTML = 'Il y a eu un problème de communication avec le serveur : <br>' + err;
         main.appendChild(errorTitle);
         main.appendChild(errorText);
     })
---
+++ frontend/scripts/product.js
@@ -0,0 +1,88 @@
+const main = document.querySelector('.main');
+const address = "http://localhost:3000/api/teddies/";
+
+const searchParams = new URL(document.URL).searchParams;
+
+//Récupère l'id dans les paramètres get de l'URL
+const getId = () => searchParams.get('id');
+
+//Récupère un item via l'API en passant l'Id récupéré
+const getItem = (id) => {
+    if (id === null) return;
+    return new Promise((resolve, reject) => {
+        fetch(address + id)
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+    });
+}
+
+const successMessage = (text) => {
+    msg = document.createElement('div');
+    msg.classList.add('alert', 'alert--success');
+    msg.innerHTML = text ? text : 'Opération réalisée avec succès.';
+    main.appendChild(msg);
+    setInterval(() => {
+        msg.remove()
+    }, 2300);
+}
+
+const displayItem = (item, container) => {
+
+    newImg = document.createElement('img');
+    newImg.classList.add('product__img');
+    newImg.setAttribute('src', item.imageUrl);
+    container.appendChild(newImg);
+
+    newTitle = document.createElement('h2');
+    newTitle.classList.add('product__title');
+    newTitle.innerHTML = item.name;
+    container.appendChild(newTitle);
+
+    newDescription = document.createElement('p');
+    newDescription.classList.add('product__description');
+    newDescription.innerHTML = item.description;
+    container.appendChild(newDescription);
+
+    newSelect = document.createElement('select');
+    newSelect.classList.add('product__select');
+    container.appendChild(newSelect);
+
+    item.colors.forEach(color => {
+        newOption = document.createElement('option');
+        newOption.innerHTML = color;
+        newSelect.appendChild(newOption);
+    });
+
+    newPrice = document.createElement('p');
+    newPrice.classList.add('product__price');
+    newPrice.innerHTML = 'Acheter ce nounours au prix de ' + (item.price / 100).toFixed(2).replace('.', ',') + ' €';
+    container.appendChild(newPrice);
+
+    newButton = document.createElement('button');
+    newButton.classList.add('button', 'product__btn');
+    newButton.innerHTML = 'Ajouter au panier<i class="fas fa-shopping-cart"></i>';
+    container.appendChild(newButton);
+
+    newButton.addEventListener('click', e => {
+        successMessage("Le produit a bien été ajouté au panier");
+    })
+};
+
+getItem(getId())
+    .then(item => {
+        container = document.createElement('section');
+        container.classList.add("product");
+        main.appendChild(container);
+
+        displayItem(item, container);
+    })
+    .catch(err => {
+        errTitle = document.createElement('h2');
+        errorTitle = document.createElement('h2');
+        errorTitle.innerHTML = 'Une erreur est survenue.';
+        errorText = document.createElement('p');
+        errorText.innerHTML = 'Il y a eu un problème de communication avec le serveur : <br>' + err;
+        main.appendChild(errorTitle);
+        main.appendChild(errorText);
+    })
